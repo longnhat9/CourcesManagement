5272b900878cba6e4f38139205d9751d158caf82
unknown <longnhatstar2002@gmail.com>
2023-09-07 09:46:38 +0700

    upload files

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -16,8 +16,26 @@ class CoursesController {
             .catch((error) => next(error));
     }
 
+    // [GET] /courses/create
     create(req, res, next) {
-        res.send('Create Course');
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    async store(req, res, next) {
+        //const formData = req.body;
+        const dataCreateCourse = req.body;
+        dataCreateCourse.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        // const course = new course({những thuộc tính ở đây mà muốn thêm vào document trong database});
+        // những thuộc tính muốn thêm vào trong document database thì trong document và model phải chứa những trường thông tin đó
+        const course = new course(dataCreateCourse);
+        await course.save()
+            .then(() => {
+                res.redirect('/'); // chuyển hướng về trang chủ (/)
+            })
+            .catch((error) => {
+                next(error)
+            })
     }
 }
 
--- src/app/models/course.js
+++ src/app/models/course.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    discription: { type: String, maxLength: 255 },
-    image: { type: String, maxLength: 800 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    name: { type: String, required: true, },
+    discription: { type: String },
+    image: { type: String, maxLength: 500 },
+    videoId: { type: String, required: true, },
+    slug: { type: String, slug: 'name', unique: true },
+    // thuộc tính ràng buộc uique: true là để tránh slug của các course trùng nhau
+    // createAt: { type: Date, default: Date.now },
+    // updateAt: { type: Date, default: Date.now },
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
